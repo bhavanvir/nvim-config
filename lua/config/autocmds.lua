b69470c47fa2d0fff08dc15cd1fd5b2ddf32c5e8
bhavanvir <bhavanvirrai@pantheon.io>
2025-06-17 08:56:00 -0700

    Update config and change tabing behaviour depending on file

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "sh", "bash", "zsh" },
+  callback = function()
+    vim.bo.tabstop = 2
+    vim.bo.shiftwidth = 2
+    vim.bo.softtabstop = 2
+    vim.bo.expandtab = true
+  end,
+})
